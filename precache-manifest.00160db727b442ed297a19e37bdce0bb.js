969dcf3722db89b9545b261ba826398c8022ef4c
vishald2509 <vishald2509@gmail.com>
2020-06-16 22:48:14 +0530

    Updates

---
+++ precache-manifest.00160db727b442ed297a19e37bdce0bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5bec2b97fe7ae767308",
+    "url": "/phone-store-react/static/css/main.505d545a.chunk.css"
+  },
+  {
+    "revision": "c5bec2b97fe7ae767308",
+    "url": "/phone-store-react/static/js/main.55518317.chunk.js"
+  },
+  {
+    "revision": "b8a15b7019804a60945d",
+    "url": "/phone-store-react/static/js/runtime~main.50df2143.js"
+  },
+  {
+    "revision": "59e7c31837f2a88e6a01",
+    "url": "/phone-store-react/static/css/2.60f94200.chunk.css"
+  },
+  {
+    "revision": "59e7c31837f2a88e6a01",
+    "url": "/phone-store-react/static/js/2.7afe2459.chunk.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/phone-store-react/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "92836886259f546859ea0e1846688aea",
+    "url": "/phone-store-react/index.html"
+  }
+];
